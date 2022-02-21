4d44d3335fdca088ff50b79b61d09f87a8f49b8d
CountFett67 <100142094+CountFett67@users.noreply.github.com>
2022-02-21 09:11:02 -0500

    Update HelloWorld.cpp

--- Ages11-14/HelloWorld.cpp
+++ Ages11-14/HelloWorld.cpp
@@ -7,3 +7,19 @@ int main(int argc, char *argv[])
     return 0;                             // Exit the program
 }
   
+
+
+New Code is here
+
+#include <iostream> // Include the Input-Output Library
+
+int main(int argc, char *argv[])
+{
+    char myString[] = "Hello World!";  
+    char myString2[] = "Hello Grayson!";  
+    
+    // Create the HelloWorld string
+    std::cout << myString << std::endl;   // Output the string to the console
+    std::cout << myString2 << std::endl;   // Output the string to the console
+    return 0;                             // Exit the program
+}
